09d094853189839ff13185ee2b6fb8b934ed8921
Sadia Tasnim <tasnimsadia259@gmail.com>
2023-08-08 16:42:15 +0600

    UI complete

---
+++ Protocol&DelegateProject/BossViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BossViewController.swift
+//  Protocol&DelegateProject
+//
+//  Created by Sadia on 8/8/23.
+//
+
+import UIKit
+
+class BossViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
--- Protocol&DelegateProject/InternViewController.swift
+++ Protocol&DelegateProject/InternViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class InternViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
