cb4e315bad395ce60292b60fa3c7e9d1282ccc97
Sadia Tasnim <tasnimsadia259@gmail.com>
2023-08-08 17:38:26 +0600

    complete protocol and delegate project

--- Protocol&DelegateProject/BossViewController.swift
+++ Protocol&DelegateProject/BossViewController.swift
@@ -7,14 +7,31 @@
 
 import UIKit
 
+protocol SelectionDelegate {
+    func didChoice(image:UIImage, backgroundColor: UIColor)
+}
+
 class BossViewController: UIViewController {
+    
+    var seletectedDelegate: SelectionDelegate!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
     }
     
-
+    @IBAction func uiKitButtonTapped(_ sender: UIButton) {
+        
+        seletectedDelegate.didChoice(image: UIImage(named: "uikit")!, backgroundColor: UIColor.orange)
+        navigationController?.popViewController(animated: true)
+    }
+    
+    
+    @IBAction func swiftUIuttonTapped(_ sender: UIButton) {
+        
+        seletectedDelegate.didChoice(image: UIImage(named: "swiftUI")!, backgroundColor: UIColor.blue)
+        navigationController?.popViewController(animated: true)
+    }
+    
 
 }
--- Protocol&DelegateProject/InternViewController.swift
+++ Protocol&DelegateProject/InternViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class InternViewController: UIViewController {
+    
+    @IBOutlet weak var selectedImageView: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    
+    @IBAction func chooseButtonTapped(_ sender: UIButton) {
+        
+        let bossViewController = self.storyboard?.instantiateViewController(withIdentifier: "BossViewController") as! BossViewController
+        bossViewController.seletectedDelegate = self
+                //secondViewController.placeImage = UIImage(named: imageName)
+                
+                self.navigationController?.pushViewController(bossViewController, animated: true)
+    }
+    
 
+}
 
+extension InternViewController: SelectionDelegate{
+    func didChoice(image: UIImage, backgroundColor: UIColor) {
+        selectedImageView.image = image
+        view.backgroundColor = backgroundColor
+    }
+    
+    
 }
 
